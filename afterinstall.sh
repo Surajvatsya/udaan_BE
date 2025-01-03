6c7c22c819c9e80f88ebc50ba57f13f7660e33f0
suraj.kumar1 <suraj.kumar1@juspay.in>
2025-01-03 19:22:34 +0530

    feat: added instructions for AWS pipeline and codeDeploy

---
+++ afterinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/ubuntu
+
+
+sudo apt-get -y update
+
+# INSTALL NODEJS - ONLY FOR UBUNTU
+sudo apt install nodejs -y
+
+# INSTALL NPM & PM2 - ONLY FOR UBUNTU
+sudo apt install npm -y
+
+
+npm install
---
+++ applicationstart.sh
@@ -0,0 +1,4 @@
+cd /home/ubuntu
+sudo npm install -g pm2
+git pull
+pm2 start ecosystem.config.js
